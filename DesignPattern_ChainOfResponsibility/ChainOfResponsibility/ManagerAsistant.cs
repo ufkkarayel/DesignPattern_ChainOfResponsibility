467ade33506193050f02e187a53da590b0cf5dfe
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2023-12-22 22:01:32 +0300

    manager asistant sınıfı oluşturuldu.

---
+++ DesignPattern_ChainOfResponsibility/ChainOfResponsibility/ManagerAsistant.cs
@@ -0,0 +1,36 @@
+﻿using DesignPattern_ChainOfResponsibility.DAL.Context;
+using DesignPattern_ChainOfResponsibility.DAL.Entities;
+using DesignPattern_ChainOfResponsibility.Models;
+
+namespace DesignPattern_ChainOfResponsibility.ChainOfResponsibility
+{
+    public class ManagerAsistant : Employee
+    {
+        public override void ProcessRequest(CustomerProcessViewModel req)
+        {
+            ChainOfResContext context = new ChainOfResContext();
+            if (req.Amount<=20000)
+            {
+                CustomerProcess customerProcess = new CustomerProcess();
+                customerProcess.Amount = req.Amount;
+                customerProcess.Name = req.Name;
+                customerProcess.EmployeeName = "Şube Müdür Yardımcısı Oğuzhan Karal";
+                customerProcess.Description = "Müşterinin talep ettiği kredi tutarı tarafımca ödenmiştir.";
+                context.CustomerProcesses.Add(customerProcess);
+                context.SaveChanges();
+            }
+            else if (NextApprover !=null)
+            {
+                CustomerProcess customerProcess=new CustomerProcess();
+                customerProcess.Amount=req.Amount;
+                customerProcess.Name = req.Name;
+                customerProcess.EmployeeName = "Şube Müdür Yardımcısı Oğuzhan Karal";
+                customerProcess.Description = "Müşterinin talep ettiği kredi tutarı tarafımca ödenemediği için işlem şube müdürüne aktarılmıştır.";
+                context.CustomerProcesses.Add(customerProcess);
+                context.SaveChanges();
+                NextApprover.ProcessRequest(req);
+            }
+           
+        }
+    }
+}
