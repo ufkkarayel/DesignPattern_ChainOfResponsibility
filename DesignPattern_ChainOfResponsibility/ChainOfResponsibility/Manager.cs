6d439bbacece42d4f2827f424b0395f8a2d09827
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2023-12-22 22:03:51 +0300

    manager sınıfı oluşturuldu

--- DesignPattern_ChainOfResponsibility/ChainOfResponsibility/Manager.cs
+++ DesignPattern_ChainOfResponsibility/ChainOfResponsibility/Manager.cs
@@ -4,33 +4,33 @@ using DesignPattern_ChainOfResponsibility.Models;
 
 namespace DesignPattern_ChainOfResponsibility.ChainOfResponsibility
 {
-    public class ManagerAsistant : Employee
+    public class Manager : Employee
     {
         public override void ProcessRequest(CustomerProcessViewModel req)
         {
             ChainOfResContext context = new ChainOfResContext();
-            if (req.Amount<=200000)
+            if (req.Amount <= 280000)
             {
                 CustomerProcess customerProcess = new CustomerProcess();
                 customerProcess.Amount = req.Amount;
                 customerProcess.Name = req.Name;
-                customerProcess.EmployeeName = "Şube Müdür Yardımcısı Oğuzhan Karal";
+                customerProcess.EmployeeName = "Şube Müdürü İpek Çelik";
                 customerProcess.Description = "Müşterinin talep ettiği kredi tutarı tarafımca ödenmiştir.";
                 context.CustomerProcesses.Add(customerProcess);
                 context.SaveChanges();
             }
-            else if (NextApprover !=null)
+            else if (NextApprover != null)
             {
-                CustomerProcess customerProcess=new CustomerProcess();
-                customerProcess.Amount=req.Amount;
+                CustomerProcess customerProcess = new CustomerProcess();
+                customerProcess.Amount = req.Amount;
                 customerProcess.Name = req.Name;
-                customerProcess.EmployeeName = "Şube Müdür Yardımcısı Oğuzhan Karal";
+                customerProcess.EmployeeName = "Şube Müdürü İpek Çelik";
                 customerProcess.Description = "Müşterinin talep ettiği kredi tutarı tarafımca ödenemediği için işlem şube müdürüne aktarılmıştır.";
                 context.CustomerProcesses.Add(customerProcess);
                 context.SaveChanges();
                 NextApprover.ProcessRequest(req);
             }
-           
+
         }
     }
 }
