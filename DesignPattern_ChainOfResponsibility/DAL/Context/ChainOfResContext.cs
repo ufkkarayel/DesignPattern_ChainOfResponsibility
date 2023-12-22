a4e7f2d20405f456e6a3005c18f134dc31eed166
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2023-12-22 21:18:52 +0300

    Add project files.

---
+++ DesignPattern_ChainOfResponsibility/DAL/Context/ChainOfResContext.cs
@@ -0,0 +1,14 @@
+﻿using DesignPattern_ChainOfResponsibility.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DesignPattern_ChainOfResponsibility.DAL.Context
+{
+    public class ChainOfResContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB; initial Catalog=ChainOfDb; integrated Security=true");
+        }
+        public DbSet<CustomerProcess> CustomerProcesses { get; set; }
+    }
+}
---
+++ DesignPattern_ChainOfResponsibility/DAL/Entities/CustomerProcess.cs
@@ -0,0 +1,12 @@
+﻿namespace DesignPattern_ChainOfResponsibility.DAL.Entities
+{
+    public class CustomerProcess
+    {
+        public int CustomerProcessID { get; set; }
+        public string Name { get; set; }
+        public decimal Amount { get; set; }
+        public string EmployeeName { get; set; }
+        public string Description { get; set; }
+
+    }
+}
