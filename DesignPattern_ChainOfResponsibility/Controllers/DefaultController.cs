e87379aa3a2feda7653d23ea8ab394ce7de616fa
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2023-12-22 22:15:54 +0300

    controller oluşturuldu

---
+++ DesignPattern_ChainOfResponsibility/Controllers/DefaultController.cs
@@ -0,0 +1,30 @@
+﻿using DesignPattern_ChainOfResponsibility.ChainOfResponsibility;
+using DesignPattern_ChainOfResponsibility.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DesignPattern_ChainOfResponsibility.Controllers
+{
+    public class DefaultController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(CustomerProcessViewModel model)
+        {
+            Employee treasurer= new Treasurer();
+            Employee managerAssistant = new ManagerAsistant();
+            Employee manager = new Manager();
+            Employee areaDirector= new AreaDirector();
+
+            treasurer.SetNextApprover(managerAssistant);
+            managerAssistant.SetNextApprover(manager);
+            manager.SetNextApprover(areaDirector);
+            treasurer.ProcessRequest(model);
+            return View();
+
+        }
+    }
+}
