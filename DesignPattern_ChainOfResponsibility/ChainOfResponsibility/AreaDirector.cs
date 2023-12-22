4d4b552eee9812e15ed04211888cf0319774ea5a
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2023-12-22 22:08:19 +0300

    AreaDirector sınıfı oluşturuldu

---
+++ DesignPattern_ChainOfResponsibility/ChainOfResponsibility/AreaDirector.cs
@@ -0,0 +1,35 @@
+﻿using DesignPattern_ChainOfResponsibility.DAL.Context;
+using DesignPattern_ChainOfResponsibility.DAL.Entities;
+using DesignPattern_ChainOfResponsibility.Models;
+
+namespace DesignPattern_ChainOfResponsibility.ChainOfResponsibility
+{
+    public class AreaDirector : Employee
+    {
+        public override void ProcessRequest(CustomerProcessViewModel req)
+        {
+            ChainOfResContext context = new ChainOfResContext();
+            if (req.Amount <= 400000)
+            {
+                CustomerProcess customerProcess = new CustomerProcess();
+                customerProcess.Amount = req.Amount;
+                customerProcess.Name = req.Name;
+                customerProcess.EmployeeName = "Bölge Müdürü Özlem Karakoç";
+                customerProcess.Description = "Müşterinin talep ettiği kredi tutarı tarafımca ödenmiştir.";
+                context.CustomerProcesses.Add(customerProcess);
+                context.SaveChanges();
+            }
+            else 
+            {
+                CustomerProcess customerProcess = new CustomerProcess();
+                customerProcess.Amount = req.Amount;
+                customerProcess.Name = req.Name;
+                customerProcess.EmployeeName = "Bölge Müdürü Özlem Karakoç";
+                customerProcess.Description = "Müşterinin talep ettiği kredi tutarı bankanın günlük ödeme limitini aştığı için işlem gerçekleştirilemedi.";
+                context.CustomerProcesses.Add(customerProcess);
+                context.SaveChanges();                
+            }
+
+        }
+    }
+}
